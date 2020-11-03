7398df6b786cd6ea931ee4ae93010ee4523a8855
Edvin Mocibob <edvin.mocibob@gmail.com>
2020-11-03 19:46:41 +0100

    Change home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import OtherPage from "./OtherPage";
@@ -7,12 +6,12 @@ import Fib from "./Fib";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <div>
+          <h1>Fib Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
-        </header>
+        </div>
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
